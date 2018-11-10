789c34097b79d5c0a884787e55d650bcd1da8d44
Liam Dowling <liamcamerondowling7@gmail.com>
2018-11-10 19:39:57 +0000

    I added function to the start button. it now progresses to the next screen of the game. This was completed with the aid of StackOverflow and youtube tutorials. This work was completed at 19:40 on 10/11/2018
    Signed-off-by: Liam Dowling <liamcamerondowling7@gmail.com>

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Game {
 
     JFrame DisplayWindow;
     Container con;
-    JPanel TitlePanel, startButtonPanel, mainTextPanel;
+    JPanel TitlePanel, startButtonPanel, mainTextPanel, choiceButtonPanel;
     JLabel TitleName;           //font name,        font style   size
     Font TitleFont = new Font("Times New Roman", Font.PLAIN,50);
-    Font ButtonFont = new Font("Times New Roman", Font.PLAIN, 30);
-    JButton startButton;
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+    JButton startButton, choice1, choice2, choice3, choice4;
+    JTextArea mainTextArea;
+
+    TitleScreenHandler TSHandler = new TitleScreenHandler();
 
     public static void main(String[] args)
     {
@@ -39,7 +44,8 @@ public class Game {
         startButton = new JButton("START");     //creates a button
         startButton.setBackground(Color.BLACK);     //setting color of button
         startButton.setForeground(Color.WHITE);     //setting color of button
-        startButton.setFont(ButtonFont);            //setting font of button
+        startButton.setFont(normalFont);            //setting font of button
+        startButton.addActionListener(TSHandler);    //adding an action listener to the button
 
         TitlePanel.add(TitleName);      //adds the text box to the panel
         startButtonPanel.add(startButton);  //adds teh button to the panel
@@ -49,6 +55,59 @@ public class Game {
     }
     public void createGameScreen()
     {
+        TitlePanel.setVisible(false);
+        startButtonPanel.setVisible(false);
 
+
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100,100,600,250);
+        mainTextPanel.setBackground(Color.BLACK);
+        con.add(mainTextPanel);
+
+        mainTextArea = new JTextArea("This is the main text area. This text will not overlap once it runs out of space");
+        mainTextArea.setBounds(100,100,600,250);
+        mainTextArea.setBackground(Color.BLACK);
+        mainTextArea.setForeground(Color.white);        //text color
+        mainTextArea.setFont(normalFont);
+        mainTextArea.setLineWrap(true);     //sets the text to go to the next line if the container is too small
+        mainTextPanel.add(mainTextArea);        //adds the text area to the panel
+
+        choiceButtonPanel = new JPanel();
+        choiceButtonPanel.setBounds(250,350,300,150);
+        choiceButtonPanel.setBackground(Color.BLACK);
+        choiceButtonPanel.setLayout(new GridLayout(4,1));   //means one column and four rows of added components
+        con.add(choiceButtonPanel);
+
+        choice1 = new JButton("Option 1");
+        choice1.setBackground(Color.BLACK);
+        choice1.setForeground(Color.white);
+        choice1.setFont(normalFont);
+        choiceButtonPanel.add(choice1);
+
+        choice2 = new JButton("Option 2");
+        choice2.setBackground(Color.BLACK);
+        choice2.setForeground(Color.white);
+        choice2.setFont(normalFont);
+        choiceButtonPanel.add(choice2);
+
+        choice3 = new JButton("Option 3");
+        choice3.setBackground(Color.BLACK);
+        choice3.setForeground(Color.white);
+        choice3.setFont(normalFont);
+        choiceButtonPanel.add(choice3);
+
+        choice4 = new JButton("Option 4");
+        choice4.setBackground(Color.BLACK);
+        choice4.setForeground(Color.white);
+        choice4.setFont(normalFont);
+        choiceButtonPanel.add(choice4);
+    }
+
+    public class TitleScreenHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            createGameScreen();
+        }
     }
 }
